935460caa3e64b5d3468253fc16b42d63b1c9018
Sam Nicholls <samstudio8+github@gmail.com>
2026-05-02 21:48:22 +0100

    where am i

--- src/burrow/tui.py
+++ src/burrow/tui.py
@@ -1,9 +1,15 @@
 from textual.app import App, ComposeResult
 from textual.binding import Binding
+from textual.widgets import Footer, Header
 
 
 class BurrowApp(App):
+    TITLE = "burrow"
     BINDINGS = [
         Binding("q", "quit", "Quit"),
         Binding("ctrl+c", "quit", "Quit"),
     ]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
